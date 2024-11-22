6d29beba87a37e2b5b2ed612a61a1937da908106
Thimo Merke <thimomerke@gmail.com>
2024-11-22 11:50:18 +0100

    Minor visual adjustments to increase visibility of sidebar elements

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -78,7 +78,7 @@ export default function Sidebar({ layerStates, toggleLayer, flyToCallback }) {
         onChange={() => toggleLayer(layerKey)}
       />
       <span>{layerStates[layerKey].label}</span>
-      {layerStates[layerKey].color != "transparent" && (
+      {layerStates[layerKey].color !== "transparent" && (
       <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1">
         <circle cx="8" cy="8" r="6" stroke="#000000" strokeWidth="1" fill={layerStates[layerKey].color} />
       </svg>
@@ -89,7 +89,7 @@ export default function Sidebar({ layerStates, toggleLayer, flyToCallback }) {
     </div>
     <div id="footer">
     <img id="cbs-logo" src="https://design.cbs.dk/wp-content/uploads/CBSlogo_extended_rgb_blue.svg" alt="CBS Logo"></img>
-    &copy;2024 Copenhagen Business School / Nordic ESG Lab, v1.1
+    <br></br>&copy;2024 Copenhagen Business School / Nordic ESG Lab, v1.2
     </div>
     </div>
   );
